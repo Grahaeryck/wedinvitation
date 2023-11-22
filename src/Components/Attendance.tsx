836c005671048146acd9a30663b85c0b8c3f0308
graha_eryck@dell.com <graha_eryck@dell.com>
2023-11-22 23:07:06 +0700

    3

--- src/Components/Attendance.tsx
+++ src/Components/Attendance.tsx
@@ -20,6 +20,7 @@ import {
   Container,
   Divider,
   Input,
+  Select,
   Text,
   theme,
 } from "@chakra-ui/react";
@@ -44,7 +45,7 @@ function Attendance({}: Props) {
                     align="center"
                 >
                     <Heading as="h1"  textColor={"#FAF9F6"}>Are you Attending?</Heading>
-                    <Text textColor={"#F9F6EE"}>Please Fill-up the form to notify you that you're attending. Thanks.</Text>
+                    <Text textColor={"#F9F6EE"}>Please Fill-up the form to notify us that you're attending. Thanks.</Text>
                     <HStack mt="2rem" gap='5'>
                         <Input 
                             bgColor={"white"} 
@@ -52,12 +53,18 @@ function Attendance({}: Props) {
                             focusBorderColor='#483C32'
                             placeholder='Name' 
                             _placeholder={{ color: '#483C32' }}/>
-                        <Input 
-                            bgColor={"white"} 
-                            opacity={'70%'} 
-                            focusBorderColor='#483C32'
-                            placeholder='Email' 
-                            _placeholder={{ color: '#483C32' }}/>
+                        <Box minW="250px">
+                            <Select 
+                                placeholder='Select Reception Session'
+                                width='100%'
+                                bgColor={"white"} 
+                                opacity={'70%'} 
+                                focusBorderColor='#483C32'
+                                >
+                              <option value='option1'>Session 1</option>
+                              <option value='option2'>Session 2</option>
+                            </Select>
+                        </Box>
                         <Button bgColor={"#F9F6EE"} variant='solid' alignItems='center' width='100%'>
                             I'AM ATTENDING
                         </Button>
